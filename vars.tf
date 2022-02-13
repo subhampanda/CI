15b886526e92e60d51022fd1657806032422497a
subhampanda <panda.subham4@gmail.com>
2022-02-14 03:58:12 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,18 @@
+variable "access_key" {
+  default = "AKIA5ZJKVQO52NGMQ43M"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-east-2"
+  }
+
+variable "db_instance_identifier"{
+    default = "database-1"
+}
+
+
+variable "db_snapshot_identifier"{
+    default = "database-snapshot"
+}
